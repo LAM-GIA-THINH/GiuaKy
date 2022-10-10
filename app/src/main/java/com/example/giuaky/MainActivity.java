08fcfe5615a33189cdd7906cbdd2409e63d285ab
LAM-GIA-THINH <giathinh961617@gmail.com>
2022-10-10 18:30:27 +0700

    checkbox

--- app/src/main/java/com/example/giuaky/MainActivity.java
+++ app/src/main/java/com/example/giuaky/MainActivity.java
@@ -3,30 +3,61 @@ package com.example.giuaky;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnlogin;
+    EditText edtUsername;
+    EditText edtPassword;
+    CheckBox cbRemember;
+    SharedPreferences sharedPreferences;
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
-        TextView signup = (TextView) findViewById(R.id.signup);
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        AnhXa();
+        sharedPreferences = getSharedPreferences("datalogin", MODE_PRIVATE);
+        edtUsername.setText(sharedPreferences.getString("taikhoan",""));
+        edtPassword.setText(sharedPreferences.getString("matkhau",""));
+        cbRemember.setChecked(sharedPreferences.getBoolean("checked",false));
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                String username = edtUsername.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+                if (username.equals("admin") && password.equals("admin")){
                     openActivity2();
                     Toast.makeText(MainActivity.this, "ĐĂNG NHẬP THÀNH CÔNG", Toast.LENGTH_SHORT).show();
+                    if (cbRemember.isChecked()) {
+
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("taikhoan", username);
+                        editor.putString("matkhau", password);
+                        editor.putBoolean("checked", true);
+                        editor.commit();
+
+
+                    } else {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.remove("taikhoan");
+                        editor.remove("matkhau");
+
+                        editor.remove("checked");
+
+                        editor.commit();
+
+                    }
                 } else
                     Toast.makeText(MainActivity.this, "ĐĂNG NHẬP THẤT BẠI", Toast.LENGTH_SHORT).show();
             }
@@ -37,8 +68,17 @@ public class MainActivity extends AppCompatActivity {
                 openActivity3();
             }
         });
-    }
 
+    }
+    private void AnhXa(){
+        btnlogin = (Button) findViewById(R.id.loginbtn);
+        edtPassword = (EditText) findViewById(R.id.password);
+        edtUsername = (EditText) findViewById(R.id.username);
+        cbRemember = (CheckBox) findViewById(R.id.checkBox);
+        signup = (TextView) findViewById(R.id.signup);
+
+
+    }
     public void openActivity2() {
         Intent intent = new Intent(this, Page.class);
         startActivity(intent);
